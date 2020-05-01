2fbdd267d5b9908af2f1ce14de2107ef57989cf5
Zulgub <juanjosealosnero@gmail.com>
2020-05-01 20:50:29 +0200

    repository link update

--- assets/includes/vistas/info.php
+++ assets/includes/vistas/info.php
@@ -5,7 +5,7 @@
         <h4 class="card-title"><?php echo $GLOBALS["name"]; ?></h4>
         <p class="card-text">
             <div class="badge badge-warning p-2">Versión: <span class="text-white">0.0.3 βeta</span></div>
-            <a class="badge badge-secondary p-2" target="_blank" href="https://github.com/Zulgub/Framework-Manager"><i class="fab fa-github"></i> Github Repository</a>
+            <a class="badge badge-secondary p-2" target="_blank" href="https://github.com/juanjo96Dev/Framework-Manager"><i class="fab fa-github"></i> Github Repository</a>
             <a role="license" target="_blank" class="badge badge-success p-2" data-toggle="tooltip" data-placement="bottom" title="Licencia creative commons by 3.0" href="https://creativecommons.org/licenses/by/3.0/" alt="Licencia creative commons by 3.0"><i class="fab fa-creative-commons"></i> <i class="fab fa-creative-commons-by"></i>
             </a><br>
             <div class="pt-2">Autor: <a target="_blank" href="http://juanjosegonzalezrodriguez.com.es/">Juan José González Rodríguez</a></div>
